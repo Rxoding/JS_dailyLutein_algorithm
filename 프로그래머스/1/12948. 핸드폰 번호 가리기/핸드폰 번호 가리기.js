033a0c336630eea84173f18050f14b3c076f3c4b
Rxoding <42318885+Rxoding@users.noreply.github.com>
2024-08-27 12:19:52 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.06 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,7 +1,5 @@
 function solution(phone_number) {
-  const frontNumber = phone_number.slice(0, -4);
   answer =
-    frontNumber.replace(frontNumber, "*".repeat(frontNumber.length)) +
-    phone_number.slice(-4);
+    "*".repeat(phone_number.slice(0, -4).length) + phone_number.slice(-4);
   return answer;
 }
