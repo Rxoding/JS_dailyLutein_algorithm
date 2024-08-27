acc7883baa6fcba1d1f3ff0d80382d6919e70b59
Rxoding <42318885+Rxoding@users.noreply.github.com>
2024-08-27 12:49:31 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.11 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,13 +1,10 @@
 function solution(numbers) {
-  const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  let newArr = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (!numbers.includes(arr[i])) {
-      newArr.push(arr[i]);
+  let hap = 0;
+  for (let i = 0; i < 10; i++) {
+    if (!numbers.includes(i)) {
+      hap += i;
     }
   }
-  console.log(newArr);
-  const hap = newArr.reduce((a, b) => a + b);
   return hap;
 }
 
