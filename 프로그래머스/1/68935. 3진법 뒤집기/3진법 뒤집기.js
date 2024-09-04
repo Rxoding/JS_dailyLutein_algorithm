f310751069c4550153baa3e1cfba79470160aaf3
Rxoding <42318885+Rxoding@users.noreply.github.com>
2024-09-04 10:47:30 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -1,17 +1,3 @@
 function solution(n) {
-  let trit = [];
-
-  // 3진법으로 변환
-  while (n > 0) {
-    trit.push(n % 3);
-    n = Math.floor(n / 3);
-  }
-
-  // 3진법 숫자를 뒤집어서 10진법으로 변환
-  let result = 0;
-  for (let i = 0; i < trit.length; i++) {
-    result += trit[i] * 3 ** (trit.length - 1 - i);
-  }
-
-  return result;
+  return parseInt(n.toString(3).split("").reverse().join(""), 3);
 }
