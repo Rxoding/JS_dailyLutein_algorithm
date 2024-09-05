492f1354164382f61e3d88c014a94841c2313fc7
Rxoding <42318885+Rxoding@users.noreply.github.com>
2024-09-05 09:41:36 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -0,0 +1,17 @@
+function solution(s) {
+  let splitS = s.split(" ");
+  let answer = "";
+  for (let i = 0; i < splitS.length; i++) {
+    for (let j = 0; j < splitS[i].length; j++) {
+      if (j === 0 || j % 2 === 0) {
+        answer += splitS[i][j].toUpperCase();
+      } else {
+        answer += splitS[i][j].toLowerCase();
+      }
+    }
+    if (i < splitS.length - 1) {
+      answer += " ";
+    }
+  }
+  return answer;
+}
