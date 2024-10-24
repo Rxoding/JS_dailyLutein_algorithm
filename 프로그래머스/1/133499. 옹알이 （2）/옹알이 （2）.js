d466de046ecbbea953a34eb6e085be5ab8bf0a95
Rxoding <42318885+Rxoding@users.noreply.github.com>
2024-10-25 05:44:39 +0900

    [level 1] Title: 옹알이 (2), Time: 0.66 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
@@ -1,17 +1,20 @@
 function solution(babbling) {
-    const babblables = ["aya", "ye", "woo", "ma"];
+  const babblables = ['aya', 'ye', 'woo', 'ma'];
 
-    return babbling.reduce((possible, babbl, index) => {
-        for (let i = 0; i < babblables.length; i += 1) {
-            if (babbl.includes(babblables[i].repeat(2))) return possible;
-        }
+  return babbling.reduce((possible, babbl) => {
+    // 연속된 발음 확인
+    for (let i = 0; i < babblables.length; i++) {
+      if (babbl.includes(babblables[i].repeat(2))) return possible;
+    }
 
-        for (let i = 0; i < babblables.length; i += 1) {
-            babbl = babbl.split(babblables[i]).join(' ').trim();
-        }
+    // 발음 할 수있는 부분 제거
+    for (let i = 0; i < babblables.length; i++) {
+      babbl = babbl.split(babblables[i]).join(' ').trim();
+    }
 
-        if (babbl) return possible;
+    // babbl 이 존재할 시 possible 리턴
+    if (babbl) return possible;
 
-        return possible += 1;
-    }, 0)
+    return (possible += 1);
+  }, 0);
 }
